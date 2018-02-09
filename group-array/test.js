c79aef345b838fcfd81c122d94ce1b96dfc0bdc8
tangj <1798516284@qq.com>
2018-02-09 16:28:50 +0800

    index学习完成

---
+++ group-array/test.js
@@ -0,0 +1,75 @@
+require('mocha');
+var groupArray = require('./');
+var assert = require('assert');
+
+describe('group-array', () => {
+    it('should throw an error when invalid args are passed:', function () {
+        assert.throws(function () {
+            groupArray('');
+        },'group-array expects an array.');
+    });
+
+    it('should return an empty array when an empty array is passed:',function () {
+        assert.deepEqual(groupArray([]),[]);
+    });
+
+    it('should create groups based on the value of the specified property', function () {
+        var arr = [{
+                tag: 'one',
+                content: 'A'
+            },
+            {
+                tag: 'one',
+                content: 'B'
+            },
+            {
+                tag: 'two',
+                content: 'C'
+            },
+            {
+                tag: 'two',
+                content: 'D'
+            },
+            {
+                tag: 'three',
+                content: 'E'
+            },
+            {
+                tag: 'three',
+                content: 'F'
+            }
+        ];
+
+        var actual = groupArray(arr, 'tag');
+
+        assert.deepEqual(actual, {
+            one: [{
+                    tag: 'one',
+                    content: 'A'
+                },
+                {
+                    tag: 'one',
+                    content: 'B'
+                }
+            ],
+            two: [{
+                    tag: 'two',
+                    content: 'C'
+                },
+                {
+                    tag: 'two',
+                    content: 'D'
+                }
+            ],
+            three: [{
+                    tag: 'three',
+                    content: 'E'
+                },
+                {
+                    tag: 'three',
+                    content: 'F'
+                }
+            ],
+        });
+    });
+});
