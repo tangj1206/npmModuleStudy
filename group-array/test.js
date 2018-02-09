39b860dedd61da7ab8663fe4de71de088d158bbb
tangj <1798516284@qq.com>
2018-02-09 16:32:11 +0800

    测试用例

--- group-array/test.js
+++ group-array/test.js
@@ -6,11 +6,15 @@ describe('group-array', () => {
     it('should throw an error when invalid args are passed:', function () {
         assert.throws(function () {
             groupArray('');
-        },'group-array expects an array.');
+        }, 'group-array expects an array.');
     });
 
-    it('should return an empty array when an empty array is passed:',function () {
-        assert.deepEqual(groupArray([]),[]);
+    it('should return an empty array when an empty array is passed:', function () {
+        assert.deepEqual(groupArray([]), []);
+    });
+
+    it('should return the original array when no grouping properties are passed:', function () {
+        assert.deepEqual(groupArray(['a', 'b', 'c']), ['a', 'b', 'c']);
     });
 
     it('should create groups based on the value of the specified property', function () {
