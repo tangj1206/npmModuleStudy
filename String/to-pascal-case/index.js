72861e0126e4fdeb1da45abb65da0eb36052ab2c
tangj1206 <1798516284@qq.com>
2018-04-04 17:31:24 +0800

    功能函数待完善

---
+++ String/to-pascal-case/index.js
@@ -0,0 +1,18 @@
+var space = require('to-space-case')
+
+/** 
+ * Export.
+ */
+
+ module.exports = toPascalCase
+
+ /**    
+  * Convert a `string` to pascal case.
+  * 
+  * @param {String} string
+  * @return {String}
+  */
+
+  function toPascalCase(string) {
+      return space(string).replace()
+  }
